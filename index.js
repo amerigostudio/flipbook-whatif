138e67edc33cc05f4000e22fd0c9a306fd6220d5
amerigostudio <luca@amerigofilm.com>
2025-11-21 20:55:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 import worker from './js/worker';
 
 $.fn.FlipBook = function(options) {
-    const parentContainer = this.length? this[0]: $('<div>').appendTo('body');
+    const parentContainer = this.length ? this[0] : $('<div>').appendTo('body');
     return worker(parentContainer, options);
 }
 
-$(()=> {
+$(() => {
+
+  // FORCE DEFAULT PDF (What If = Japanese first)
+  const defaultPdf = 'amf_Simply-what-if_JP.pdf';
+
   let containers = $('.flip-book-container');
-  for(let i = 0; i<containers.length; ++i) {
-    const jContainer = $(containers[i]), src = jContainer.attr('src');
-    if(!!src) {
-      jContainer.FlipBook({pdf: src});
+
+  if (containers.length === 0) {
+    // no container with src → create one automatically
+    const auto = $('<div class="flip-book-container">')
+      .attr('src', defaultPdf)
+      .appendTo('body');
+
+    auto.FlipBook({ pdf: defaultPdf });
+
+  } else {
+    // normal behavior (used by Sycamore)
+    for (let i = 0; i < containers.length; ++i) {
+      const jContainer = $(containers[i]), src = jContainer.attr('src');
+      const pdfToLoad = src || defaultPdf;
+      jContainer.FlipBook({ pdf: pdfToLoad });
     }
   }
+
 });
 
 window.jQuery = window.$ = $;
